1b33d2cb0ddebbb54d325aa62bb7a09413ffd52f
elzup <guild0105@gmail.com>
2026-06-12 12:26:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from fastapi import FastAPI
-import random  # randomライブラリを追加
 
 app = FastAPI()
 
@@ -14,7 +13,6 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-
 @app.get("/omikuji")
 def omikuji():
     omikuji_list = [
@@ -22,12 +20,27 @@ def omikuji():
         "中吉",
         "小吉",
         "吉",
-        "半吉",
+        "秀吉",
         "末吉",
         "末小吉",
         "凶",
         "小凶",
         "大凶"
     ]
+    
+    return {"result" : omikuji_list[random.randrange(10)]}
 
-    return omikuji_list[random.randrange(10)]
+@app.get("/index")
+def index(request: Request):
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+            <p>youser ip (request.client.host): """ + request.client.host + """</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
